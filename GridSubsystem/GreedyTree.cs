9518ca2352834434b89c8c65bc3788e6ee8a056b
Rudra <rudrak1996@gmail.com>
2018-10-08 19:12:27 +1100

    Commit Before Adding Node

---
+++ GridSubsystem/GreedyTree.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GridSubsystem
+{
+    class GreedyTree
+    {
+        #region Private Members 
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Public Members
+        #endregion
+
+        #region Private Members 
+        #endregion
+
+    }
+}
--- GridSubsystem/Program.cs
+++ GridSubsystem/Program.cs
@@ -13,15 +13,12 @@ namespace GridSubsystem
             Console.WriteLine("Hello World!!");
             Grid grid = new Grid(3, 3);
             List<string> words = new List<string> {"CAT", "AXE", "BOX"};
-            //Console.WriteLine(grid.InsertWord(new Word("CAT", new Position(0, 0), Orientation.Horizontal)));
+            Console.WriteLine(grid.InsertWord(new Word("BOX", new Position(0, 1), Orientation.Vertical)));
             List<Word> possibleWords = grid.GenPossiblePositions(words);
-            //Console.WriteLine(grid.InsertWord(new Word("CAT", new Position(0, 0), Orientation.Horizontal)));
-            //Console.WriteLine(grid.InsertWord(new Word("AXE", new Position(0, 1), Orientation.Vertical)));
-            //Console.WriteLine(grid.InsertWord(new Word("XN", new Position(1, 1), Orientation.Vertical)));
+
             foreach (Word word in possibleWords)
             {
                 Console.WriteLine(String.Format("Word {0}, Orientation: {1}, Position: {2}", word.ToString(), word.Orientation.ToString(), word.Position.ToString()));
-
             }
             Console.WriteLine(grid.ToString());
 
